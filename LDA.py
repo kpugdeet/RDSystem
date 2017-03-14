d78b07c60e61f8206b92f2bce75568e0e2a6fc2d
kpugdeet <krittaphat.pug@gmail.com>
2017-03-14 10:52:49 -0400

    Update request

--- LDA.py
+++ LDA.py
@@ -1,18 +1,15 @@
 
 class LDA:
     def __init__(self):
-        self.rankList = {}
         print ("Initial LDA")
 
     def calRanking (self, metaCard, userPref):
-        print metaCard
-        print userPref
-
-        self.rankList["0"] = "Document 0 is a document"
-        self.rankList["1"] = "Document 1 is a document"
-        self.rankList["2"] = "Document 2 is a document"
-        self.rankList["3"] = "Document 3 is a document"
-        return self.rankList
+        rankList = {}
+        rankList["0"] = "Document 0 is a document"
+        rankList["1"] = "Document 1 is a document"
+        rankList["2"] = "Document 2 is a document"
+        rankList["3"] = "Document 3 is a document"
+        return rankList
 
     def train (self, itemID, itemDes):
         print ("Train LDA")
@@ -20,6 +17,11 @@ class LDA:
 
         # Calculate topic distribution of each item
         for row in itemID:
-            itemValue.append('1,2,3,0')
+            itemValue.append('1,2,3,10')
 
         return itemValue
+
+    def calDistri (self, text):
+        result = '1,2,3,4'
+
+        return result
--- Main.py
+++ Main.py
@@ -22,41 +22,71 @@ class query:
 
     def POST(self):
         data = json.loads(web.data())
+        conn = sqlite3.connect("user.db", check_same_thread=False)
+        c = conn.cursor()
+
+        # Get user preference list
         rw.acquire_read()
         try:
-            # Get user preference list
-            conn = sqlite3.connect("user.db", check_same_thread=False)
-            c = conn.cursor()
-            for row in c.execute("SELECT userPref.itemID, itemDetail.itemValue FROM userPref JOIN itemDetail ON userPref.itemID = itemDetail.itemID "
+            for row in c.execute("SELECT itemDetail.itemValue FROM userPref JOIN itemDetail ON userPref.itemID = itemDetail.itemID "
                                  "WHERE userID = '{0}';".format(data['userID'])):
-                self.userPref.append([int(x) for x in row[1].split(",")])
-            # Make recommendation list
-            rankResult = lda.calRanking(data['metaCard'], np.array(self.userPref))
+                self.userPref.append([int(x) for x in row[0].split(",")])
         finally:
             rw.release_read()
 
+        # Make recommendation list
+        rankResult = lda.calRanking(data['metaCard'], np.array(self.userPref))
+
+        # Return result in json
         web.header('Content-Type', 'application/json')
         return json.dumps(rankResult)
 
-# Update when user choose new metaCard
 class update:
     def __init__(self):
         self.output = {"status": "Update Done"}
 
     def POST(self):
         data = json.loads(web.data())
-        rw.acquire_write()
-        try:
-            conn = sqlite3.connect("user.db", check_same_thread=False)
-            c = conn.cursor()
-            for row in c.execute("SELECT * FROM userPref WHERE userID = '{0}' and itemID = '{1}'".format(data['userID'], data['itemID'])):
-                self.output = {"status": "Data already exist"}
+        conn = sqlite3.connect("user.db", check_same_thread=False)
+        c = conn.cursor()
+
+        for tmp in data["item"]:
+            # Update to itemDetail db
+            rw.acquire_read()
+            try:
+                exist = c.execute("SELECT itemID FROM itemDetail WHERE itemID = {0} LIMIT 1;".format(tmp["itemID"]))
+            finally:
+                rw.release_read()
+            for row in exist:
                 break
             else:
-                c.execute("INSERT INTO userPref (userID, itemID) VALUES ('{0}','{1}')".format(data['userID'], data['itemID']))
-            conn.commit()
-        finally:
-            rw.release_write()
+                rw.acquire_write()
+                try:
+                    c.execute("INSERT INTO itemDetail (itemID, itemDes, itemValue) VALUES ('{0}','{1}','{2}')"
+                              .format(tmp['itemID'], tmp['itemDes'], lda.calDistri(tmp['itemDes'])))
+                    conn.commit()
+                finally:
+                    rw.release_write()
+
+            # Check data exist
+            rw.acquire_read()
+            try:
+                exist = c.execute("SELECT userID FROM userPref WHERE userID = '{0}' and itemID = '{1}' LIMIT 1;".format(data['userID'], tmp['itemID']))
+            finally:
+                rw.release_read()
+
+            # If not exist insert to db
+            for row in exist:
+                break
+            else:
+                rw.acquire_write()
+                try:
+                    c.execute("INSERT INTO userPref (userID, itemID) VALUES ('{0}','{1}')".format(data['userID'], tmp['itemID']))
+                    conn.commit()
+                finally:
+                    rw.release_write()
+
+        # Return status
         return self.output
 
 class train:
@@ -66,26 +96,34 @@ class train:
         self.itemDes = []
 
     def POST(self):
-        rw.acquire_write()
-        try:
-            conn = sqlite3.connect('user.db', check_same_thread=False)
-            c = conn.cursor()
+        conn = sqlite3.connect('user.db', check_same_thread=False)
+        c = conn.cursor()
 
-            # Training new value for each item
+        # Training new value for each item
+        rw.acquire_read()
+        try:
             for row in c.execute("SELECT itemID, itemDes FROM itemDetail;"):
                 self.itemID.append(row[0])
                 self.itemDes.append(row[1])
-            itemValue = lda.train(self.itemID, self.itemDes)
+        finally:
+            rw.release_read()
 
-            c.executescript('drop table if exists itemDetail;')
-            c.execute("CREATE TABLE itemDetail (itemID text, itemDes text, itemValue text)")
-            for i in range(len(self.itemID)):
-                c.execute("INSERT INTO itemDetail VALUES ('{0}','{1}','{2}');".format(self.itemID[i], self.itemDes[i], itemValue[i]))
-            conn.commit()
+        # Train
+        itemValue = lda.train(self.itemID, self.itemDes)
 
-            time.sleep(10)
+        # Write back to db
+        rw.acquire_write()
+        try:
+           c.executescript('drop table if exists itemDetail;')
+           c.execute("CREATE TABLE itemDetail (itemID text, itemDes text, itemValue text)")
+           for i in range(len(self.itemID)):
+               c.execute("INSERT INTO itemDetail VALUES ('{0}','{1}','{2}');".format(self.itemID[i], self.itemDes[i], itemValue[i]))
+           conn.commit()
+           time.sleep(10)
         finally:
             rw.release_write()
+
+        # Return status
         return self.output
 
 if __name__ == "__main__":
